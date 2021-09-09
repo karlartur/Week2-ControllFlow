74f05a7e7f006a3e75c7454c050d8bdb0f3d1520
Opilane TTHK <opilane@tthk.ee>
2021-09-09 11:15:27 +0300

    Add Control Flow

---
+++ PositiveNegative/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm palub kasutajal sisestada numbrit -10 kuni 10
+            //programm kontrollib numbrit
+            //kui number on positiivne, konsool kuvab "positive"
+            //kui number on negatiivne, końsool kuvab "negative"
+            //kuinumber on 0, siis konsool kuvab"null"
+
+            Console.WriteLine("Sisesta number -10 kuni 10:");
+            int UserNumber = Convert.ToInt32(Console.ReadLine());
+            if (UserNumber > 0)
+            {
+                Console.WriteLine("Positive.");
+            }
+
+            else if (UserNumber < 0)
+            {
+                Console.WriteLine("Negative.");
+            }
+            
+            else 
+            {
+                Console.WriteLine("Null.");
+            }
+
+        }
+    }
+}
